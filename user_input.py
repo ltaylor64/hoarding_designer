906ac90289ddd8c74b30967cf418803ee4d681e8
ltaylor64 <72707850+ltaylor64@users.noreply.github.com>
2020-10-26 20:48:56 +0000

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,25 +1,11 @@
 import hoarding_functions
 
+test = "test2.txt"
 
-def wind_print_bs5975(wind_pressure, probability_factor, town_country, shore_distance, exposure_factor,
-                      basic_wind_velocity, topographical_factor, altitude, s_wind):
-    text = "Surface Wind Load (BS 5975):\n\n"
-    text += "Probability factor; cprob = " + str(probability_factor) + "\n"
-    text += "Site in town or country = " + town_country + "\n"
-    text += "Distance from shoreline = " + str(shore_distance) + "km\n"
-    text += "Combined exposure factor; ce = " + str(exposure_factor) + "\n"
-    text += "Basic wind velocity; vb,map = " + str(basic_wind_velocity) + "\n"
-    text += "Topographical factor; Twind = " + str(topographical_factor) + "\n"
-    text += "Site altitude; A = " + str(altitude) + "m\n"
-    text += "Air density; Ad = 1.225kg/m^3\n\n"
-    text += "Swind = Twind * vb,map * (1 + (A / 1000)) = " + str(s_wind) + "\n\n"
-    text += "Peak velocity pressure; qb = 0.5 * Ad * (cprob)^2 * (ce)^2 * (Swind)^2 = " + str(wind_pressure) + "kN/m^2"
-    file.write(text)
+# file = open("Temporary Hoarding Calculation Pack.txt", "w")
+file = open(test, "w")
 
-
-file = open("Temporary Hoarding Calculation Pack.txt", "w")
-
-print("-" * 50, "\nTemporary Site Hoarding Designer v0.0.1", "\n", "-" * 50, sep="")
+print("-" * 50, "\nTemporary Site Hoarding Designer v0.0.2", "\n", "-" * 50, sep="")
 
 # Project details
 # project_name = input("Project name: ")
@@ -54,27 +40,46 @@ while True:
 h_udl = {"high": 1.5, "medium": 0.74, "low": 0.37, "none": 0}
 h_udl = h_udl.get(crowd_load)
 print("\nProgramme calculates wind load using two separate methods (BS 5975 & BS 5950). User to decide upon "
-      "method used once both have been calculated. BS 5950 method ---BROKEN---")
+      "method used once both have been calculated")
 print()
+
 print("BS 5975 Wind Loading Method:")
 
 peak_pressure_bs5975, p_factor_bs5975, town_country_bs5975, distance_shore_bs5975, exposure_factor_bs5975, \
     basic_wind_velocity_bs5975, topographical_factor_bs5975, altitude_bs5975, s_wind_bs5975, \
     = hoarding_functions.wind_bs5975(height)
 
-print("BS5975 peak velocity pressure = " + str(round(peak_pressure_bs5975, 2)) + "kN/m^2")
 print()
+print("BS 6399 Wind Loading Method:")
+
+p_factor_bs6399, town_bs6399, upwind_distance_sea_bs6399, upwind_distance_town_bs6399, basic_wind_velocity_bs6399, altitude_bs6399, height_bs6399,\
+    building_type_factor_bs6399, dynamic_augmentation_factor_bs6399, effective_height_bs6399, displacement_height_bs6399, site_altitude_factor_bs6399, interpolated_sc_bs6399, interpolated_st_bs6399, interpolated_tc_bs6399, \
+    interpolated_tt_bs6399, average_height_rooftops_bs6399, spacing_surrounding_buildings_bs6399, terrain_building_factor_bs6399, \
+    effective_wind_speed_bs6399, direction_factor_bs6399, seasonal_factor_bs6399, site_wind_speed_bs6399, dynamic_pressure_bs6399 = hoarding_functions.wind_bs6399(height)
+
+# Choose whether BS5975 method or BS6399 method used for calculations
 
 choose_wind = None
 while True:
     if (choose_wind == "1") or (choose_wind == "2"):
         if choose_wind == "1":
-            wind_print_bs5975(peak_pressure_bs5975, p_factor_bs5975, town_country_bs5975, distance_shore_bs5975,
-                              exposure_factor_bs5975, basic_wind_velocity_bs5975, topographical_factor_bs5975,
-                              altitude_bs5975, s_wind_bs5975)
+            hoarding_functions.wind_print_bs5975(file, peak_pressure_bs5975, p_factor_bs5975, town_country_bs5975,
+                                                 distance_shore_bs5975, exposure_factor_bs5975,
+                                                 basic_wind_velocity_bs5975, topographical_factor_bs5975,
+                                                 altitude_bs5975, s_wind_bs5975)
             break
         else:
-            # wind_print_bs5950()
+            hoarding_functions.wind_print_bs6399(file, p_factor_bs6399, town_bs6399, upwind_distance_sea_bs6399,
+                                                 upwind_distance_town_bs6399, basic_wind_velocity_bs6399,
+                                                 altitude_bs6399, height_bs6399, building_type_factor_bs6399,
+                                                 dynamic_augmentation_factor_bs6399, effective_height_bs6399,
+                                                 displacement_height_bs6399, site_altitude_factor_bs6399,
+                                                 interpolated_sc_bs6399, interpolated_st_bs6399, interpolated_tc_bs6399,
+                                                 interpolated_tt_bs6399, average_height_rooftops_bs6399,
+                                                 spacing_surrounding_buildings_bs6399, terrain_building_factor_bs6399,
+                                                 effective_wind_speed_bs6399, direction_factor_bs6399,
+                                                 seasonal_factor_bs6399, site_wind_speed_bs6399,
+                                                 dynamic_pressure_bs6399)
             break
     else:
         choose_wind = input("Wind calculation choice between BS 5975 & BS 5950 (BS 5975 = 1; BS 5950 = 2): ")
